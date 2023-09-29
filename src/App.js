8dd7578417cc64a2511282bacade7732a8ee0592
Yasin Rezaei <yassinrezaei1366@gmail.com>
2023-09-29 12:25:02 +0330

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-function App{
-    return(
-        <div>
-            <h1>new react</h1>
-            <p>this is a new react project</p>
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <h1>new react</h1>
+      <p>this is a new react project</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
